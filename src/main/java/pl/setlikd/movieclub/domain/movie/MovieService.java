c3580a05945a8fb7e9dcbba3d8e77a3705bdf48e
daniel <hetman.set@gmail.com>
2022-07-05 15:58:57 +0200

    Impl adding a new movie

--- src/main/java/pl/setlikd/movieclub/domain/movie/MovieService.java
+++ src/main/java/pl/setlikd/movieclub/domain/movie/MovieService.java
@@ -1,7 +1,12 @@
 package pl.setlikd.movieclub.domain.movie;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.setlikd.movieclub.domain.genre.Genre;
+import pl.setlikd.movieclub.domain.genre.GenreRepository;
 import pl.setlikd.movieclub.domain.movie.dto.MovieDto;
+import pl.setlikd.movieclub.domain.movie.dto.MovieSaveDto;
+import pl.setlikd.movieclub.storage.FileStorageService;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,9 +15,14 @@ import java.util.Optional;
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final FileStorageService fileStorageService;
+    private final GenreRepository genreRepository;
 
-    public MovieService(MovieRepository movieRepository) {
+
+    public MovieService(MovieRepository movieRepository, FileStorageService fileStorageService, GenreRepository genreRepository) {
         this.movieRepository = movieRepository;
+        this.fileStorageService = fileStorageService;
+        this.genreRepository = genreRepository;
     }
 
     public List<MovieDto> findAllPromotedMovies() {
@@ -30,4 +40,25 @@ public class MovieService {
                 .map(MovieDtoMapper::map)
                 .toList();
     }
+
+    @Transactional
+    public void addMovie(MovieSaveDto movieToSave) {
+        Movie movie = new Movie();
+        movie.setTitle(movieToSave.getTitle());
+        movie.setOriginalTitle(movieToSave.getOriginalTitle());
+        movie.setPromoted(movieToSave.isPromoted());
+        movie.setReleaseYear(movieToSave.getReleaseYear());
+        movie.setShortDescription(movieToSave.getShortDescription());
+        movie.setDescription(movieToSave.getDescription());
+        movie.setYoutubeTrailerId(movieToSave.getYoutubeTrailerId());
+        Genre genre = genreRepository.findByNameIgnoreCase(movieToSave.getGenre()).orElseThrow();
+        movie.setGenre(genre);
+        if (movieToSave.getPoster() != null) {
+            String savedFileName = fileStorageService.saveImage(movieToSave.getPoster());
+            movie.setPoster(savedFileName);
+        }
+        movieRepository.save(movie);
+    }
+
+
 }
