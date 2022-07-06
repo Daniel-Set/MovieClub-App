91ce9a6c39abfaa0f6bc6c441f67f0da0f37fe5f
daniel <hetman.set@gmail.com>
2022-07-06 16:07:20 +0200

    Register-form

--- src/main/java/pl/setlikd/movieclub/domain/user/dto/UserCredentialsDto.java
+++ src/main/java/pl/setlikd/movieclub/domain/user/dto/UserCredentialsDto.java
@@ -1,4 +1,20 @@
 package pl.setlikd.movieclub.domain.user.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
 public class UserCredentialsDto {
+    private final String email;
+    private final String password;
+    private final Set<String> roles;
+
+    public UserCredentialsDto(String email, String password, Set<String> roles) {
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
 }
--- src/main/java/pl/setlikd/movieclub/domain/user/dto/UserRegistrationDto.java
+++ src/main/java/pl/setlikd/movieclub/domain/user/dto/UserRegistrationDto.java
@@ -1,4 +1,11 @@
 package pl.setlikd.movieclub.domain.user.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserRegistrationDto {
+    private String email;
+    private String password;
 }
